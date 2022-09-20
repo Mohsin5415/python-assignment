14b213bd9548af46f33139fbdbc1fc6a4cf5c45d
MOHSIN RATHOD <110963546+Mohsin5415@users.noreply.github.com>
2022-09-20 09:52:45 +0530

    Add files via upload

---
+++ sdlc.py
@@ -0,0 +1,7 @@
+# what is software ? -----> Solution Of Problems,Collection Of Programs
+
+# What Are The Types Of Applications ? -----> (1)Desktop Application (2)Web Application (3)Mobile Application
+
+# What Is Progrraming ? ------->Programming Is The Process Of Creating A Set Of Instruction That Tell A Computer How To Perform A Task
+
+# What Is Phython ? ---->Python Is An Interpreted,Object Oriented,High Level Programming Language with Dynamic Semantics
